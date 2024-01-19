f50ab4cb19d4ed4569ebc2b1aca9f7fe28d0e684
MarkEddy001 <MarkEddy254@gmail.com>
2024-01-19 03:11:13 -0800

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,10 @@
-# Install a specific version of flask (2.1.0)
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+#This puppet mainfest installs flask from pip3
+package { 'python3-pip':
+  ensure => 'installed',
+}
+
+exec { 'install flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
   unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
 }
