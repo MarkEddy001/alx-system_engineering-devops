55761e9e102ec8f8c6aec258d6d23c4a86688ddc
MarkEddy001 <MarkEddy254@gmail.com>
2024-01-19 03:09:59 -0800

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
 # Install a specific version of flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 freeze | /bin/grep Flask | /bin/grep 2.1.0',
 }
