e1e2a00a332dc070aa6d3285a41e9acccadafcd3
MarkEddy001 <MarkEddy254@gmail.com>
2024-01-19 17:37:47 -0800

    install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,30 +1,13 @@
 # Using Puppet, install flask from pip3.
 
-# Requirements:
 
-# Install flask
-# Version must be 2.1.0
-
-package {
-    'python3.8':
-    ensure  => '3.8.10',
+package { 'python3-pip':
+  ensure => installed,
 }
 
-package {
-    'python3-pip':
-    ensure  => installed,
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
 
-package {
-    'flask':
-    ensure   => '2.1.0',
-    require  => Package['python3-pip'],
-    provider => 'pip',
-}
-
-package {
-    'Werkzeug':
-    ensure   => '2.1.0',
-    require  => Package['python3-pip'],
-    provider => 'pip',
-}
