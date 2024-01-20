e3bed050fd0dfe908bdabe3771ce71c60289a5f0
MarkEddy001 <MarkEddy254@gmail.com>
2024-01-19 18:24:28 -0800

    Hii task 1 inabooo mazee

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,30 @@
 # Using Puppet, install flask from pip3.
 
+# Requirements:
 
-package { 'python3-pip':
-  ensure => installed,
+# Install flask
+# Version must be 2.1.0
+
+package {
+    'python3.8':
+    ensure  => '3.8.10',
 }
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+package {
+    'python3-pip':
+    ensure  => installed,
 }
 
+package {
+    'flask':
+    ensure   => '2.1.0',
+    require  => Package['python3-pip'],
+    provider => 'pip',
+}
+
+package {
+    'Werkzeug':
+    ensure   => '2.1.0',
+    require  => Package['python3-pip'],
+    provider => 'pip',
+}
