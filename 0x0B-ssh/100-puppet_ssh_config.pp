eb0bd99c11b29137aa6c56ab6fbba5a2df84a5ba
MarkEddy001 <MarkEddy254@gmail.com>
2024-01-19 19:32:46 -0800

    set up your client SSH configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#This puppet mainfest configures using private key ~/.ssh/school
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
