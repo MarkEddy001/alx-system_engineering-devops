70d8363b809ef43ca37e663b13a7d5977de0f590
Wanyoike Edwards <57659295+MarkEddy001@users.noreply.github.com>
2024-03-09 22:44:51 +0300

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# fix 500 error 
-exec { 'wordpress typo error':
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path     => '/usr/bin:/usr/sbin:/bin',
+# fix 500 error
+exec {'fix500':
+  command => 'sed -i "s|require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );\
+|require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );|g" /var/www/html/wp-settings.php',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
 }
