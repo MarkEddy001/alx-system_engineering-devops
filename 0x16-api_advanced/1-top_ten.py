472e81b454bde66518d113c577a12b8c67af26d5
MarkEddy001 <Markeddy254@gmail.com>
2024-05-08 00:46:11 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""A module that retrieves the tiltes of hot subreddit."""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    """a function that queries the Reddit API and prints the titles of the
+    first 10 hot posts listed for a given subreddit."""
+
+    headers = {"User-Agent": "Middle-Chipmunk-3601"}
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers=headers,
+        allow_redirects=False,
+    )
+    if response.status_code == 200:
+        result = response.json()
+        for i in range(10):
+            print(result['data']['children'][i]['data']['title'])
+    else:
+        print(None)
