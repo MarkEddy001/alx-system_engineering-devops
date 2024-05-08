2e5305a5009ff05217c622464dfb1e134ad3d256
MarkEddy001 <Markeddy254@gmail.com>
2024-05-08 00:12:41 +0000

    function that queries the Reddit API and returns the number of subscribers (not active users, total subscribers) for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,23 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
         'User-Agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'
     }
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            if 'data' in data:
+                return data['data']['subscribers']
+            else:
+                print("Invalid JSON response - 'data' key not found")
+                return 0
+        except ValueError:
+            print("Invalid JSON response")
+            return 0
+    else:
+        print("Request failed with status code: {}".format(response.status_code))
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
